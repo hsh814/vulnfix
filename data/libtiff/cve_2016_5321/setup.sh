90cc44703fa0e2ed0f995cf14ed4d5f768f32d27
hsh814 <hsh814@naver.com>
2024-01-17 06:11:55 +0000

    Update libtiff bugzilla_2611 - cve_2017_5225

--- data/libtiff/cve_2016_5321/setup.sh
+++ data/libtiff/cve_2016_5321/setup.sh
@@ -8,9 +8,9 @@ popd
 
 rm -rf smake_source && mkdir smake_source
 pushd smake_source
-  ../source/configure
-  /home/yuntong/vulnfix/thirdparty/smake/smake --init
-  /home/yuntong/vulnfix/thirdparty/smake/smake CFLAGS="-static -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="-static -fsanitize=address -fsanitize=undefined -g" LDFLAGS="-fsanitize=address -fsanitize=undefined" -j10
+  CC=clang CXX=clang++ ../source/configure
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake --init
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake CFLAGS="-static -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="-static -fsanitize=address -fsanitize=undefined -g" LDFLAGS="-fsanitize=address -fsanitize=undefined" -j10
 popd
 
 rm -rf sparrow-out && mkdir sparrow-out
@@ -22,17 +22,16 @@ rm -rf sparrow-out && mkdir sparrow-out
 
 rm -rf dafl_source && mkdir dafl_source
 pushd dafl_source
-  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/bugzilla_2611/sparrow-out/bug/slice_func.txt" \
-  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/bugzilla_2611/sparrow-out/bug/slice_dfg.txt" \
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/cve_2016_5321/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/cve_2016_5321/sparrow-out/bug/slice_dfg.txt" \
   ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
-  CMAKE_EXPORT_COMPILE_COMMANDS=1 CFLAGS="-DFORTIFY_SOURCE=2 -fno-omit-frame-pointer -fsanitize=address -ggdb -Wno-error" \
-  CXXFLAGS="$CFLAGS" ../source/configure
+  CMAKE_EXPORT_COMPILE_COMMANDS=1 ../source/configure
 
-  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/bugzilla_2611/sparrow-out/bug/slice_func.txt" \
-  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/bugzilla_2611/sparrow-out/bug/slice_dfg.txt" \
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/cve_2016_5321/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/cve_2016_5321/sparrow-out/bug/slice_dfg.txt" \
   ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
-  make CFLAGS="-ldl -lutil -fsanitize=address -ggdb -Wno-error" CXXFLAGS="-fsanitize=address -ldl -lutil -ggdb -Wno-error" \
-  LDFLAGS=" -ldl -lutil -fsanitize=address" -j 10
+  make CFLAGS="-static -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="-static -fsanitize=address -fsanitize=undefined -g" LDFLAGS="-fsanitize=address -fsanitize=undefined" -j10
+
 popd
 
 rm -rf raw_build && mkdir raw_build
@@ -41,5 +40,5 @@ pushd raw_build
   make CFLAGS="-static -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="-static -fsanitize=address -fsanitize=undefined -g" -j10
 popd
 
-cp raw_build/tools/tiffmedian ../tiffmedian
-cp dafl_source/tools/tiffmedian ../tiffmedian.instrumented
+cp raw_build/tools/tiffcrop ./tiffcrop
+cp dafl_source/tools/tiffcrop ./tiffcrop.instrumented
--- data/libtiff/cve_2016_9532/setup.sh
+++ data/libtiff/cve_2016_9532/setup.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
-
+rm -rf source
 git clone https://github.com/vadz/libtiff.git
 mv libtiff source
-cd source/
-git checkout d651abc
+pushd source
+  git checkout d651abc
+popd
 
-./configure
-# not using UBSAN as it triggers another bug in POC before the bug in this CVE
-make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+rm -rf smake_source && mkdir smake_source
+pushd smake_source
+  CC=clang CXX=clang++ ../source/configure
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake --init
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
 
-cp tools/tiffcrop ../
+rm -rf sparrow-out && mkdir sparrow-out
+/home/yuntong/vulnfix/thirdparty/sparrow/bin/sparrow -outdir ./sparrow-out \
+-frontend "cil" -unsound_alloc -unsound_const_string -unsound_recursion -unsound_noreturn_function \
+-unsound_skip_global_array_init 1000 -skip_main_analysis -cut_cyclic_call -unwrap_alloc \
+-entry_point "main" -max_pre_iter 10 -slice "bug=tiffcrop.c:2978" \
+./smake_source/sparrow/tools/tiffcrop/*.i
+
+rm -rf dafl_source && mkdir dafl_source
+pushd dafl_source
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/cve_2016_9532/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/cve_2016_9532/sparrow-out/bug/slice_dfg.txt" \
+  ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
+  CMAKE_EXPORT_COMPILE_COMMANDS=1 ../source/configure
+
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/libtiff/cve_2016_9532/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/libtiff/cve_2016_9532/sparrow-out/bug/slice_dfg.txt" \
+  ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
+  make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
+
+rm -rf raw_build && mkdir raw_build
+pushd raw_build
+  ../source/configure
+  make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
+
+cp raw_build/tools/tiffcrop ./tiffcrop
+cp dafl_source/tools/tiffcrop ./tiffcrop.instrumented
