3e2988c340b87c27d889570a43b177da08df50d4
hsh814 <hsh814@naver.com>
2024-01-17 08:51:49 +0000

    Add zziplib

--- data/zziplib/cve_2017_5976/setup.sh
+++ data/zziplib/cve_2017_5976/setup.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
-
+rm -rf source
 git clone https://github.com/gdraheim/zziplib.git
 mv zziplib source
-cd source/
-git checkout 3a4ffcd
-cd docs/
-wget https://github.com/LuaDist/libzzip/raw/master/docs/zziplib-manpages.tar
-cd ../
+pushd source
+  git checkout 3a4ffcd
+  pushd docs
+    wget https://github.com/LuaDist/libzzip/raw/master/docs/zziplib-manpages.tar
+  popd
+popd
 
-./configure
-make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+rm -rf smake_source && mkdir smake_source
+pushd smake_source
+  CC=clang CXX=clang++ ../source/configure
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake --init
+  CC=clang CXX=clang++ /home/yuntong/vulnfix/thirdparty/smake/smake CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
 
-version_dir="$(uname -s)_$(uname -r)_$(uname -m).d"
-# finalize the parameterized config file
-sed -i "s/<parameter-dir>/$version_dir/g" ../config
+rm -rf sparrow-out && mkdir sparrow-out
+/home/yuntong/vulnfix/thirdparty/sparrow/bin/sparrow -outdir ./sparrow-out \
+-frontend "cil" -unsound_alloc -unsound_const_string -unsound_recursion -unsound_noreturn_function \
+-unsound_skip_global_array_init 1000 -skip_main_analysis -cut_cyclic_call -unwrap_alloc \
+-entry_point "main" -max_pre_iter 10 -slice "bug=memdisk.c:248" \
+./smake_source/sparrow/bins/unzzipcat-mem/*.i
 
-cp $version_dir/bins/unzzipcat-mem ../
+rm -rf dafl_source && mkdir dafl_source
+pushd dafl_source
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/zziplib/cve_2017_5976/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/zziplib/cve_2017_5976/sparrow-out/bug/slice_dfg.txt" \
+  ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
+  CMAKE_EXPORT_COMPILE_COMMANDS=1 ../source/configure
+
+  DAFL_SELECTIVE_COV="/home/yuntong/vulnfix/data/zziplib/cve_2017_5976/sparrow-out/bug/slice_func.txt" \
+  DAFL_DFG_SCORE="/home/yuntong/vulnfix/data/zziplib/cve_2017_5976/sparrow-out/bug/slice_dfg.txt" \
+  ASAN_OPTIONS=detect_leaks=0 CC=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast CXX=/home/yuntong/vulnfix/thirdparty/DAFL/afl-clang-fast++ \
+  make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
+
+rm -rf raw_build && mkdir raw_build
+pushd raw_build
+  ../source/configure
+  make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" -j10
+popd
+
+cp raw_build/bins/unzzipcat-mem ./unzzipcat-mem
+cp dafl_source/bins/unzzipcat-mem ./unzzipcat-mem.instrumented
