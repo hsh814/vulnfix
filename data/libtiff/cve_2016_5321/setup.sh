e702e9f2cf612171f36aed51d916ff7f423c381f
hsh814 <hsh814@naver.com>
2024-01-25 09:15:22 +0000

    Generate distance and rebuild the benchmark

--- data/libtiff/cve_2016_5321/setup.sh
+++ data/libtiff/cve_2016_5321/setup.sh
@@ -44,10 +44,22 @@ popd
 export AFLGO=/home/yuntong/vulnfix/thirdparty/aflgo
 rm -rf aflgo_build && mkdir aflgo_build
 pushd aflgo_build
+  # first build
   mkdir temp
   TMP_DIR=$PWD/temp
   echo "tiffcrop.c:994" > $TMP_DIR/BBtargets.txt
   ADDITIONAL_FLAGS="-targets=$TMP_DIR/BBtargets.txt -outdir=$TMP_DIR -flto -fuse-ld=gold -Wl,-plugin-opt=save-temps"
+  AFL_PATH=$AFLGO CC=$AFLGO/afl-clang-fast CXX=$AFLGO/afl-clang-fast++ ../source/configure
+  AFL_PATH=$AFLGO CC=$AFLGO/afl-clang-fast CXX=$AFLGO/afl-clang-fast++ make CFLAGS="$ADDITIONAL_FLAGS -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="$ADDITIONAL_FLAGS -fsanitize=address -fsanitize=undefined -g" -j10
+  # generate distance
+  cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq > $TMP_DIR/BBnames2.txt \
+            && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
+  cat $TMP_DIR/BBcalls.txt | sort | uniq > $TMP_DIR/BBcalls2.txt \
+            && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
+  $AFLGO/scripts/genDistance.sh $PWD $TMP_DIR tiffcrop
+  # second build
+  make clean
+  ADDITIONAL_FLAGS="-distance=$TMP_DIR/distance.cfg.txt"
   AFL_PATH=$AFLGO CC=$AFLGO/afl-clang-fast CXX=$AFLGO/afl-clang-fast++ ../source/configure
   AFL_PATH=$AFLGO CC=$AFLGO/afl-clang-fast CXX=$AFLGO/afl-clang-fast++ make CFLAGS="$ADDITIONAL_FLAGS -fsanitize=address -fsanitize=undefined -g" CXXFLAGS="$ADDITIONAL_FLAGS -fsanitize=address -fsanitize=undefined -g" -j10
 popd
@@ -55,3 +67,4 @@ popd
 
 cp raw_build/tools/tiffcrop ./tiffcrop
 cp dafl_source/tools/tiffcrop ./tiffcrop.instrumented
+cp aflgo_build/tools/tiffcrop ./tiffcrop.aflgo
