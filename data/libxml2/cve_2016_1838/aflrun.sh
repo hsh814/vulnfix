3cbd842ae768208e3ad35a9fc341818995c244d5
hsh814 <hsh814@naver.com>
2024-10-24 04:51:44 +0000

    Update libxml2, zziplib, coreutils

---
+++ data/libxml2/cve_2016_1838/aflrun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# rm -rf source
+# unzip source
+# mv libxml2-cbb271655cadeb8dbb258a64701d9a3a0c4835b4 source
+
+export AFLRUN=/home/yuntong/vulnfix/thirdparty/AFLRun
+rm -rf aflrun_build && mkdir aflrun_build
+pushd aflrun_build
+  mkdir temp
+  TMP_DIR=$PWD/temp
+  mkdir aflrun_tmp
+  export AFLRUN_TMP=$PWD/aflrun_tmp
+  echo "parser.c:9837" > $TMP_DIR/BBtargets.txt
+  export AFLRUN_BB_TARGETS=$TMP_DIR/BBtargets.txt
+  export AFLRUN_TARGETS="xmllint"
+  # export ADDITIONAL_FLAGS="-flto -fuse-ld=gold -Wl,-plugin-opt=save-temps"
+  export FORCE_UNSAFE_CONFIGURE=1
+  CC=$AFLRUN/afl-clang-lto CXX=$AFLRUN/afl-clang-lto++ ASAN_OPTIONS=detect_leaks=0 ../source/autogen.sh
+  CC=$AFLRUN/afl-clang-lto CXX=$AFLRUN/afl-clang-lto++ ASAN_OPTIONS=detect_leaks=0 make CFLAGS="-static -fsanitize=address -g" CXXFLAGS="-static -fsanitize=address -g" LDFLAGS="-fsanitize=address" -j10
+popd
+
+cp aflrun_build/xmllint ./xmllint.aflrun
