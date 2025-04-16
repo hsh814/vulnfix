35d0325ff2956272917d5d92bf45d5838de511b7
hsh814 <hsh814@naver.com>
2025-04-16 08:25:52 +0000

    Update clean_26545.py

--- data/clean_26545.py
+++ data/clean_26545.py
@@ -61,7 +61,10 @@ original_files = {
     "pacfix_val",
     "cludafl-runtime",
     "dafl2_all",
+    "help.txt"
 }
+
+original_file_25003 = {"help.txt", 'pacfix', 'build.sh', 'sparrow-out', 'split.aflrun', 'run-cludafl-single.sh', 'smake_source', 'README.txt', 'runtime', 'pacfix_val', 'rerun.sh~', 'repair-out', 'temp', 'cludafl.sh', 'dafl_all', 'split.instrumented', 'dafl_val', 'aflrun.sh', 'seed', 'cludafl-runtime', 'split.pacfix.c', 'setup.sh', 'dev.patch', 'dafl_samples', 'pacfuzz_samples', 'run-afl.sh', 'dafl2_all', 'run-rafl.sh', 'run-aflrun-single.sh', 'dafl_source', 'split.orig.c', 'aflrun_build', 'live_variables', 'exploit', 'source', 'pacfix.sh', 'rerun.sh', 'config', 'cludafl_out', 'seed_parallel', 'dummy'}
 
 def remove_all_files(dir: str):
     files = os.listdir(dir)
@@ -82,6 +85,24 @@ for file in files:
     if os.path.isdir(full):
         continue
     if file in original_files:
+        continue
+    remove_count += 1
+    print(f"{remove_count}/{total}) Removing {file.encode('utf-8', errors='replace').decode('utf-8')}")
+    try:
+        os.remove(os.fsencode(full))
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(e)
+
+files = os.listdir("/home/yuntong/vulnfix/data/coreutils/gnubug_25003")
+remove_count = 0
+total = len(files)
+for file in files:
+    full = os.path.join("/home/yuntong/vulnfix/data/coreutils/gnubug_25003", file)
+    if os.path.isdir(full):
+        continue
+    if file in original_file_25003:
         continue
     remove_count += 1
     print(f"{remove_count}/{total}) Removing {file.encode('utf-8', errors='replace').decode('utf-8')}")
