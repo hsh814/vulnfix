204678dea93911b5bb119c2a6247d46cd33d2891
hsh814 <hsh814@naver.com>
2025-05-12 08:17:33 +0000

    Update get_val_external.py to save exploit, use multiprocessing

--- data/get_val_external.py
+++ data/get_val_external.py
@@ -7,6 +7,7 @@ import string
 import threading
 import subprocess
 from tqdm import tqdm
+import concurrent.futures
 
 base_path = "/home/yuntong/vulnfix/data/"
 seed_save_base_dir = base_path
@@ -20,6 +21,7 @@ orig_env = os.environ.copy()
 valfile = ""
 target = ""
 max_threads = 30
+max_processes = 30
 out_postfix = ""
 
 def generate_random_filename(dirs):
@@ -51,9 +53,10 @@ def run_target(testcase_path, testcase_name):
         else:
             shutil.copy2(testcase_path, temp_file)
             cmd = valuation_exe + " " + command.replace("<exploit>", temp_file)
-            result = subprocess.run(cmd, env=temp_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=base_path, shell=True, timeout=30)
+            result = subprocess.run(cmd, env=temp_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.path.join(base_path, "evocatio-runtime"), shell=True, timeout=30)
     except subprocess.TimeoutExpired as e:
-        os.remove(pacfix_filename)
+        if os.path.exists(pacfix_filename):
+            os.remove(pacfix_filename)
         return -1, ""
     finally:
         if os.path.exists(temp_file):
@@ -62,13 +65,16 @@ def run_target(testcase_path, testcase_name):
     err_result = result.stderr.decode('utf-8', errors='ignore')
     return ret_code, err_result
 
-def get_line():
+def get_line(sample_dir: str = ""):
     global target
     rc, errlog = run_target(base_path + "/exploit", "exploit")
     print(errlog)
-    target = extract_file_and_line(errlog)
+    if sample_dir == "":
+        target = extract_file_and_line(errlog)
+    else:
+        dest_file = os.path.join(sample_dir, "neg", "exploit")
+        shutil.move(valfile + out_postfix + "exploit", dest_file)
     # os.remove(valfile + "exploit")
-
 
 def initialize():
     if len(sys.argv) != 4:
@@ -94,6 +100,7 @@ def initialize():
     config_path = base_path + "/config"
     valuation_exe = base_path + "/runtime/"
     valfile = base_path + "/val_file"
+    os.makedirs(os.path.join(base_path, "evocatio-runtime"), exist_ok=True)
 
     with open(config_path, 'r') as config:
         lines = config.readlines()
@@ -125,9 +132,9 @@ def get_testcases(target_dir):
     files = [f for f in files if os.path.isfile(os.path.join(target_dir, f))]
     return files
 
-def thread_worker(testcase_file, pbar, sample_dir, out_dir):
+def process_worker(testcase_file, sample_dir, out_dir) -> bool:
     try:
-        ret_code, err_result = run_target(out_dir + "/" + testcase_file, testcase_file)
+        ret_code, err_result = run_target(os.path.join(out_dir, testcase_file), os.path.basename(testcase_file))
         if ret_code != -1:
             is_neg = False
             dest_dir = sample_dir + "/pos/" + testcase_file
@@ -135,12 +142,12 @@ def thread_worker(testcase_file, pbar, sample_dir, out_dir):
                 is_neg = True
                 dest_dir = sample_dir + "/neg/" + testcase_file
             shutil.move(valfile + out_postfix + str(testcase_file), dest_dir)
+            return True
     except Exception as e:
         print(f"Run ID {testcase_file} failed with error: {e}")
         import traceback
         traceback.print_exc()
-    finally:
-        pbar.update(1)
+    return False
 
 def create_directory(dir_path):
     if os.path.exists(dir_path):
@@ -154,21 +161,20 @@ if __name__ == "__main__":
     initialize()
     out_dir = os.path.join(seed_save_base_dir, subject, "seeds")
     print(f"Out: {out_dir}")
-    aflrun_tc = get_testcases(out_dir)
-    sample_dir = os.path.join(base_path, "evocatio-samples", f"{out_postfix}")
+    
+    sample_dir = os.path.join(base_path, "afl-samples", f"{out_postfix}")
     create_directory(sample_dir)
     create_directory(os.path.join(sample_dir, "pos"))
     create_directory(os.path.join(sample_dir, "neg"))
+    get_line(sample_dir)
 
-    threads = []
-    with tqdm(total=len(aflrun_tc), desc="Processing AFLRun Cases") as pbar:
-        for i, testcase_file in enumerate(aflrun_tc):
-            t = threading.Thread(target=thread_worker, args=(testcase_file, pbar, sample_dir, out_dir))
-            threads.append(t)
-            t.start()
-
-            while threading.active_count() > max_threads:
-                pass
-
-        for t in threads:
-            t.join()
+    aflrun_tc = get_testcases(out_dir)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_processes) as executor:
+        futures = [executor.submit(process_worker, tc_file, sample_dir, out_dir) for tc_file in aflrun_tc]
+    
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(aflrun_tc), desc=f"processing {subject}"):
+            try:
+                result = future.result()
+            except Exception as e:
+                tqdm.write(f"A process generated an exception {e}")
+        
